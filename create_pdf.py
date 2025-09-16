8111ff618d4c1242def18a50171485329e683024
alechten <alechten@gmail.com>
2025-09-16 10:03:48 -0500

    Update create_pdf.py

--- create_pdf.py
+++ create_pdf.py
@@ -1,6 +1,7 @@
 # Commented out IPython magic to ensure Python compatibility.
 # %pip install reportlab
 
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 from io import BytesIO
