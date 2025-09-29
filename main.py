1d7a58ee536b4d67b7ef64baba9055a22c8d3477
alechten <alechten@gmail.com>
2025-09-29 16:32:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1018,11 +1018,11 @@ class BridgeCalculatorApp:
             # Update status
             self.update_status("Analysis completed successfully - Generate PDF for detailed results")
             
-            messagebox.showinfo("Beam 1 Self Weight", f"{analysis_results.defl_obj.defl_self_wt[0,:]}")
-            messagebox.showinfo("Beam 1 Stage 1 and Stage 2 Noncomposite Weight", f"{analysis_results.defl_obj.defl_NC_S1_S2[0,:]}")
-            messagebox.showinfo("Beam 1 Stage 1 and Stage 2 Composite Weight", f"{analysis_results.defl_obj.defl_C_S1_S2[0,:]}")
-            messagebox.showinfo("Beam 1 Stage 3 Partially Composite Weight", f"{analysis_results.defl_obj.defl_PC_S3[0,:]}")
-            messagebox.showinfo("Beam 1 Stage 3 Composite Weight", f"{analysis_results.defl_obj.defl_C_S3[0,:]}")
+            messagebox.showinfo("Beam 1 Self Weight", f"{analysis_results.final_haunch_obj.defl_self_wt_adj[:,0]}")
+            messagebox.showinfo("Beam 1 Stage 1 and Stage 2 Noncomposite Weight", f"{analysis_results.defl_obj.defl_NC_S1_S2[:,0]}")
+            messagebox.showinfo("Beam 1 Stage 1 and Stage 2 Composite Weight", f"{analysis_results.defl_obj.defl_C_S1_S2[:,0]}")
+            messagebox.showinfo("Beam 1 Stage 3 Partially Composite Weight", f"{analysis_results.defl_obj.defl_PC_S3[:,0]}")
+            messagebox.showinfo("Beam 1 Stage 3 Composite Weight", f"{analysis_results.defl_obj.defl_C_S3[:,0]}")
             
             # Show results summary
             self._show_results_summary(analysis_results)
